1670e71ea97815d0748f212caf5b9c598269f644
Jim Crist <jiminy.crist@gmail.com>
2017-10-17 18:05:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from setuptools import setup
 
 setup(name='conda-pack',
       version='0.0.1',
+      url='https://github.com/jcrist/conda-pack',
+      maintainer='Jim Crist',
+      maintainer_email='jiminy.crist@gmail.com',
       license='BSD',
+      description='Package conda environments for redistribution',
+      install_requires=['click'],
       packages=['conda_pack'],
       entry_points='''
         [console_scripts]
