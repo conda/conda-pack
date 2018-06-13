47c8974050be3cf3bc05647ba76af9533b13006b
Travis CI User <travis@example.org>
2018-06-13 23:43:33 +0000

    Update docs after building Travis build 35 of conda/conda-pack
    The docs were built from the branch 'master' against the commit
    c063354503ee777e1033d31a2a05d1151ec3c8c2.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/conda/conda-pack/jobs/392011342.
    
    The doctr command that was run is
    
        /home/travis/miniconda/bin/doctr deploy . --built-docs docs/build/html/

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1+7.g4b42f66',
+    VERSION: '0.0.1+8.gc063354',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
