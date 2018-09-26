7d5c28f9859db11ab94a9814cc79d75366fa9fba
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-09-26 18:04:15 +0000

    Update docs after building Travis build 82 of conda/conda-pack
    The docs were built from the branch 'master' against the commit
    60a814326c1200998362e856040d1a9d6b98d7dc.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/conda/conda-pack/jobs/433659482.
    
    The doctr command that was run is
    
        /home/travis/miniconda/bin/doctr deploy . --built-docs docs/build/html/

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.0+1.gf15da7b',
+    VERSION: '0.2.0+2.g60a8143',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
