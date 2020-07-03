c1d12b0315cb516397b1ffdcc19eca151d9b2f6b
Michael C. Grant <mcg@cvxr.com>
2020-07-03 16:51:36 -0500

    Update setup_envs.bat

--- testing/setup_envs.bat
+++ testing/setup_envs.bat
@@ -53,3 +53,6 @@ mkdir %env%\etc\conda\activate.d
 mkdir %env%\etc\conda\deactivate.d
 copy %cwd%\extra_scripts\conda_pack_test_activate.bat %env%\etc\conda\activate.d
 copy %cwd%\extra_scripts\conda_pack_test_deactivate.bat %env%\etc\conda\deactivate.d
+
+echo Creating nested environment
+call conda.bay env create -f %ymls%\nopython.yml" -p %envs%\py36\envs\nested
