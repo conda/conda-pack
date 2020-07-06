01d336789350c71b523ea66a0d05f16420c773f1
mcg1969 <mcg1969@users.noreply.github.com>
2020-07-06 19:55:46 +0000

    deploy: b7fc0106cba199b765f1ed22ed862d8519b7d54b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.0+47.g38b816f',
+    VERSION: '0.4.0+49.gb7fc010',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
