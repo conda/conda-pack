c46cf3c4fc2c2a9a377828519e45f456f1cd4b46
Daniel Bast <2790401+dbast@users.noreply.github.com>
2021-05-18 11:19:51 +0200

    Switch to Exception, if mksquashfs is not found
    This way the cleanup of tempfiles still runs.

--- conda_pack/formats.py
+++ conda_pack/formats.py
@@ -364,9 +364,8 @@ class SquashFSArchive(ArchiveBase):
     def __init__(self, fileobj, target_path, arcroot, n_threads, verbose=False,
                  compress_level=4):
         if shutil.which("mksquashfs") is None:
-            print("Command 'mksquashfs' not found. Please install it, "
-                  "e.g. 'conda install squashfs-tools'.")
-            sys.exit(127)
+            raise SystemError("Command 'mksquashfs' not found. Please install it, "
+                              "e.g. 'conda install squashfs-tools'.")
         # we don't need fileobj, just the name of the file
         self.target_path = target_path
         self.arcroot = arcroot
