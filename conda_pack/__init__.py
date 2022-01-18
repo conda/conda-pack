edcb6094151557710993272d496a7010610f7f7a
Daniel Bast <2790401+dbast@users.noreply.github.com>
2022-01-18 14:03:55 +0100

    Update versioneer
    Done via:
    * pip install -U versioneer
    * versioneer install

--- conda_pack/__init__.py
+++ conda_pack/__init__.py
@@ -2,6 +2,5 @@ from __future__ import print_function, absolute_import
 
 from .core import CondaEnv, File, CondaPackException, pack
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
