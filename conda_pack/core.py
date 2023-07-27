4426c03baa81ccb665584d63577db4020c339760
Andrew Shamis <andrew.shamis@quantco.com>
2023-07-27 13:13:12 +0200

    convert path not prefix

--- conda_pack/core.py
+++ conda_pack/core.py
@@ -1001,11 +1001,11 @@ if __name__ == '__main__':
     else:
         script_dir = os.path.dirname(__file__)
         new_prefix = os.path.abspath(os.path.dirname(script_dir))
-        if on_win:
-            new_prefix = new_prefix.replace('\\\\', '/')
         for path, placeholder, mode in _prefix_records:
-            update_prefix(os.path.join(new_prefix, path), new_prefix,
-                          placeholder, mode=mode)
+            new_path = os.path.join(new_prefix, path)
+            if on_win:
+                new_path = new_path.replace('\\\\', '/')
+            update_prefix(new_path, new_prefix, placeholder, mode=mode)
 """
 
 
