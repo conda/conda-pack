88dcd42dd5009946fe452dd16bff20ff928b4bb3
Uwe L. Korn <uwe.korn@quantco.com>
2024-08-15 12:08:57 +0200

    Remove Python 3.7 classifier

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
