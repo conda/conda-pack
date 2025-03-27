bed8ea61d01e440419a161ea027d54ffbc97e8dc
Ivan Danov <idanov@users.noreply.github.com>
2025-03-27 11:49:41 +0000

    Make conda-pack use the new conda plugin mechanism

---
+++ conda_pack/plugin.py
@@ -0,0 +1,12 @@
+import conda.plugins
+from conda.base.context import context
+from conda_pack.cli import main
+
+
+@conda.plugins.hookimpl
+def conda_subcommands():
+    yield conda.plugins.CondaSubcommand(
+        name="pack",
+        action=main,
+        summary="Package an existing conda environment into an archive file.",
+    )
